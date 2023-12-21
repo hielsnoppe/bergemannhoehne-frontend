1bb05bff743bc0dc4e46829a04d010c2dc53ac69
Niels Hoppe <mail@nielshoppe.de>
2023-12-21 16:09:06 +0100

    Add Poker exercise for stacks

---
+++ docs/html/10_JavaScript/08_Queues_Stacks/poker.js
@@ -0,0 +1,97 @@
+// See https://stackoverflow.com/a/43053803
+const cartesian = (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
+
+// See https://stackoverflow.com/a/12646864
+function shuffle (array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+const ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A' ];
+const suits = ['diams', 'hearts', 'spades', 'clubs'];
+const stages = ['init', 'preflop', 'flop', 'turn', 'river'];
+
+var game;
+var html;
+
+const init = () => {
+    game = new Game();
+    apply();
+}
+
+const apply = () => {
+    const e_dump = document.getElementById('dump');
+    const e_table = document.getElementById('table');
+    const e_hand = document.getElementById('hand');
+
+    e_hand.innerHTML = '';
+    game.players[0].hand.forEach((card) => {
+        e_hand.innerHTML += '<li>' + card.toHTML('front') + '</li>';
+    })
+
+    e_dump.innerHTML = '';
+    game.dump.forEach((card) => {
+        e_dump.innerHTML += '<li>' + card.toHTML('back') + '</li>';
+    })
+
+    e_table.innerHTML = '';
+    game.table.forEach((card) => {
+        e_table.innerHTML += '<li>' + card.toHTML('front') + '</li>';
+    })
+}
+
+const deal = () => {
+    switch (game.stage) {
+        case 'init':
+            game.players.forEach((player) => {
+                game.burnCard();
+                game.dealCard(player);
+                game.dealCard(player);
+            });
+            game.stage = 'preflop';
+            break;
+        case 'preflop':
+            game.burnCard();
+            game.turnCard();
+            game.turnCard();
+            game.turnCard();
+            game.stage = 'flop';
+            break;
+        case 'flop':
+            game.burnCard();
+            game.turnCard();
+            game.stage = 'turn';
+            break;
+        case 'turn':
+            game.burnCard();
+            game.turnCard();
+            game.stage = 'river';
+            break;
+        case 'river':
+        default:
+            break;
+    }
+    apply();
+}
+
+class Card {
+    constructor (rank, suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    toHTML = (side) => {
+        switch (side) {
+            case 'back':
+                return `<div class="card back"></div>`;
+            case 'front':
+            default:
+                return `<div class="card rank-${this.rank} ${this.suit}">
+                    <span class="rank">${this.rank}</span>
+                    <span class="suit">&${this.suit};</span>
+                    </div>`;
+        }
+    }
+}
